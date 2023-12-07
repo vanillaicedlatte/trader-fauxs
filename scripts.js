7d923e9127cb0b39e15540c8f68e56130677ef67
vanillaicedlatte <jspann11@yahoo.com>
2023-12-07 00:52:35 -0600

    Progress on page layout and styling

---
+++ scripts.js
@@ -0,0 +1,86 @@
+availableProducts = [
+    {
+        name: "Beef Bulgogi",
+        price: "$12.99",
+        size: "16 Oz",
+        category: "Entrées & Sides",
+        image: "assets/tj-beef-bulgogi.png"
+    },
+    {
+        name: "Cheddar Cheese with Scotch Bonnet Chili Peppers",
+        price: "$9.99",
+        size: "1 Lb",
+        category: "Wedges, Wheels, Loaves, Logs",
+        image: "assets/tj-cheddar.png"
+    },
+    {
+        name: "Double Chocolate Croissants",
+        price: "$5.49",
+        size: "12 Oz",
+        category: "Cool Desserts",
+        image: "assets/tj-chocolate-croissants.png"
+    },
+    {
+        name: "Cinnamon Coffee Cake",
+        price: "$5.99",
+        size: "16 Oz",
+        category: "Sweet Stuff",
+        image: "assets/tj-cinnamon-coffee-cake.png"
+    },
+    {
+        name: "Cold Brew Coffee & Boba Coconut Non-Dairy Dessert",
+        price: "$3.49",
+        size: "16 Fl Oz",
+        category: "Cool Desserts",
+        image: "assets/tj-coffee-boba-ice-cream.png"
+    },
+    {
+        name: "Gnocchi Alla Romana",
+        price: "$4.49",
+        size: "14.1 Oz",
+        category: "Appetizers",
+        image: "assets/tj-gnocchi-alla-romana.png"
+    },
+    {
+        name: "Baked Pizza Flavored Pillow Crisps",
+        price: "$1.99",
+        size: "3.5 Oz",
+        category: "Chips, Crackers & Crunchy Bites",
+        image: "assets/tj-pizza-crisps.png"
+    },
+    {
+        name: "Roasted Garlic & Pesto Pizza with Deep Fried Crust",
+        price: "$5.99",
+        size: "14.1 Oz",
+        category: "Entrées & Sides",
+        image: "assets/tj-roasted-garlic-pizza.png"
+    },
+    {
+        name: "Sesame Miso Salad with Salmon",
+        price: "$6.99",
+        size: "10.25 Oz",
+        category: "Salads, Soups & Sides",
+        image: "assets/tj-salmon-salad.png"
+    },
+    {
+        name: "Greek Spanakopita",
+        price: "$4.49",
+        size: "14.1 Oz",
+        category: "Appetizers",
+        image: "assets/tj-spanakopita.png"
+    },
+    {
+        name: "Sweet Potato Fries",
+        price: "$2.49",
+        size: "15 Oz",
+        category: "Entrées & Sides",
+        image: "assets/tj-sweet-potato-fries.png"
+    },
+    {
+        name: "Turkey Burgers",
+        price: "$3.79",
+        size: "16 Oz",
+        category: "Entrées & Sides",
+        image: "assets/tj-turkey-burgers.png"
+    }
+]
