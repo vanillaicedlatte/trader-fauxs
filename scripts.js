4be4cd6bd2e920ad68a36551b1ea5d4f49ad5e04
vanillaicedlatte <jspann11@yahoo.com>
2023-12-11 12:49:34 -0600

    Progress on styling and populated products

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-availableProducts = [
+const availableProducts = [
     {
         name: "Beef Bulgogi",
         price: "$12.99",
@@ -83,4 +83,50 @@ availableProducts = [
         category: "Entrées & Sides",
         image: "assets/tj-turkey-burgers.png"
     }
-]
+];
+
+function addAvailableProducts (products){
+    const productsContainer = document.getElementById("products");
+
+    products.forEach(product => {
+
+        const productName = product.name;
+        const productPrice = product.price;
+        const productSize = product.size;
+        const productCategory = product.category;
+        const productImage = product.image;
+
+        productsContainer.insertAdjacentHTML('beforeend',
+            `<div class="single-product-container">
+            <div style="background-image: url(${productImage});" class="single-product-img"></div>
+            <div class="single-product-content">
+                <div class="single-product-cat-title"> <div class="single-product-category">
+                    ${productCategory}
+                </div>
+                <div class="single-product-title">
+                    ${productName}
+                </div>
+                </div>
+                <div class="single-product-pricing-add">
+                <div class="single-product-pricing">
+                    <span class="price">${productPrice}</span>/<span class="weight">${productSize}</span>
+                </div>
+                <div class="single-product-add">
+                    <button>
+                        <div class="single-product-add-button-before"></div>
+                        <div class="single-product-add-button">
+                        <svg class="Icon_icon__1UNFx" width="22" height="22" viewBox="0 0 22 22" fill="#AF0006" xmlns="http://www.w3.org/2000/svg"><path fill="#AF0006" d="M13 6V10.999L18 11V13L13 12.999V18H11V12.999L6 13V11L11 10.999V6H13Z"></path></svg> 
+                        <span>Add to List</span>
+                        </div>
+                        <div class="single-product-add-button-after"></div>
+                    </button>
+                </div>
+                </div>
+            </div>
+            </div>`
+        );
+        
+    });
+}
+
+addAvailableProducts(availableProducts);
